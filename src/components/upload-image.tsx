711d464f9507cabea853e50ed1576d86929b1c22
IaraMoscovich <imosco2007@gmail.com>
2024-08-20 09:06:53 -0300

    hola

--- src/components/upload-image.tsx
+++ src/components/upload-image.tsx
@@ -2,7 +2,7 @@
 
 import { createBrowser } from './supabase_client_client';
 import { ChangeEvent } from 'react';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 // Componente ImageUploader
 export function ImageUploader() {
