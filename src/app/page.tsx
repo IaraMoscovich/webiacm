174deaec387fb4483962140f358bb51ea081f3f4
IaraMoscovich <imosco2007@gmail.com>
2024-08-20 14:18:41 -0300

    Holaa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,6 +63,7 @@ export default async function Home () {
                     height={100}
                   />
               </a>
+              <Profile/>
           </div>
       </main>
     </body>
