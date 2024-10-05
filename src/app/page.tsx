f3f791e6109c5f8b2284ccf3036c13e0f18b6ce4
frrannn <110538082+frrannn@users.noreply.github.com>
2024-10-05 18:53:15 -0300

    cambios page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-
 import React, { useState, useEffect } from "react";
-import Profile from '@/components/upload-image';
 import { ChangeEvent } from 'react';
 
 const DashboardPage: React.FC = () => {
@@ -9,12 +7,12 @@ const DashboardPage: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [result, setResult] = useState<{ positivos: number; negativos: number, image: string } | null>(null);
 
-    useEffect(() => {
-        const resultados = localStorage.getItem('resultados');
-        if (resultados) {
-            setResult(JSON.parse(resultados));
-        }
-    }, []);
+  useEffect(() => {
+    const resultados = localStorage.getItem('resultados');
+    if (resultados) {
+      setResult(JSON.parse(resultados));
+    }
+  }, []);
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files ? event.target.files[0] : null;
@@ -22,84 +20,80 @@ const DashboardPage: React.FC = () => {
       const reader = new FileReader();
       reader.onload = () => {
         setImage(reader.result as string);
-        result?.positivos
-        result?.negativos
+        result?.positivos;
+        result?.negativos;
       };
       reader.readAsDataURL(file);
     }
   };
 
   const handleImageRemove = () => {
-    setImage(null);
+    setImage(null); // Quitar la imagen seleccionada
   };
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
-    // Manejar el evento de carga de archivos
-    const uploadFile = async (event: ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        const bucket = "FotosDB";
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = () => {
-              setImage(reader.result as string);
-              result?.positivos
-              result?.negativos
-            };
-            reader.readAsDataURL(file);
-        }
-           
-        const handleImageRemove = () => {
-            setImage(null);
-          };
 
-        // Crear FormData
-        const formData = new FormData();
-        formData.append('file', file as Blob); // Agregar el archivo al FormData
+  const uploadFile = async (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    const bucket = "FotosDB";
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImage(reader.result as string);
+        result?.positivos;
+        result?.negativos;
+      };
+      reader.readAsDataURL(file);
+    }
 
-        // Enviar FormData al servidor, http://localhost:8000https://fastapi-example-endl.onrender.com/upload-image/
-        try {
-            const response = await fetch ('http://localhost:8000/upload-image/', {
-                method: 'POST',
-                body: formData,
-            });
+    const formData = new FormData();
+    formData.append('file', file as Blob);
 
-            if (!response.ok) {
-                throw new Error('Failed to upload file.');
-            }
+    try {
+      const response = await fetch('http://localhost:8000/upload-image/', {
+        method: 'POST',
+        body: formData,
+      });
 
-            const json_res = await response.json();
+      if (!response.ok) {
+        throw new Error('Failed to upload file.');
+      }
 
-            // Guardar resultados en localStorage
-            localStorage.setItem('resultados', JSON.stringify({
-                positivos: json_res.positivos,
-                negativos: json_res.negativos,
-                imagen: json_res.image
-            }));
-            
+      const json_res = await response.json();
 
-            alert('File uploaded successfully!');
-            window.location.reload();
-        } catch (error) {
-            alert('Error uploading file.');
-            console.error('Error uploading file:', error);
-        }
-    };
-    
+      localStorage.setItem('resultados', JSON.stringify({
+        positivos: json_res.positivos,
+        negativos: json_res.negativos,
+        imagen: json_res.image,
+      }));
 
-
+      alert('File uploaded successfully!');
+      window.location.reload();
+    } catch (error) {
+      alert('Error uploading file.');
+      console.error('Error uploading file:', error);
+    }
+  };
 
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <header className="bg-pink-400 text-white p-4 flex justify-between items-center relative">
+    <div
+      className="min-h-screen p-4"
+      style={{
+        backgroundImage: 'url(/imagenes/fondo.png)', // Ruta de tu imagen de fondo
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+    >
+      <header className="bg-[#EA95C4] text-white p-4 flex justify-between items-center relative rounded-lg"> {/* Cambiado a color EA95C4 */}
         <h1 className="text-lg">¡Bienvenida, María Fernanda!</h1>
         <button className="text-white text-3xl" onClick={toggleMenu}>
           {menuOpen ? "✕" : "☰"}
         </button>
 
         {menuOpen && (
-          <div className="absolute top-0 left-0 w-full bg-pink-400 p-4 z-10 flex justify-between items-center">
+          <div className="absolute top-0 left-0 w-full bg-[#EA95C4] p-4 z-10 flex justify-between items-center rounded-lg"> {/* Cambiado a color EA95C4 */}
             <h1 className="text-lg">¡Bienvenida, María Fernanda!</h1>
             <nav className="flex space-x-4">
               <a href="#" className="text-white">Home</a>
@@ -114,60 +108,56 @@ const DashboardPage: React.FC = () => {
       </header>
 
       <main className="flex flex-col items-center">
-        <div className="bg-white shadow-md rounded-lg overflow-hidden my-4 p-4 w-full max-w-4xl">
-          <div className="flex justify-between items-center">
-            <div className="w-1/2 p-2">
-              {image ? (
-                <div>
-                  <img src={image} alt="imagen" />
-                </div>
-              ) : (
-                <div className="h-48 bg-gray-200 rounded-lg flex items-center justify-center">
-          
-                </div>
-              )}
-            </div>
-            <div className="w-1/2 p-2 bg-pink-200 rounded-lg text-center">
-              <div className="text-lg font-semibold">Dashboard</div>
-              <div className="text-3xl mt-2"> Ki-67 Positivos {result?.positivos} </div>
-              <div className="text-3xl mt-2"> Ki-67 Negativos {result?.negativos} </div>
-              <div className="text-3xl mt-2">
-              Células positivas {Math.round((result?.positivos / (result?.positivos + result?.negativos)) * 100)}%
+        <div className="flex justify-between items-center w-full max-w-4xl my-4">
+          <div className="flex flex-col w-1/2 p-2">
+            {image ? (
+              <div className="h-80 bg-gray-200 rounded-lg flex items-center justify-center">
+                <img src={image} alt="imagen" className="w-full h-full object-cover rounded-lg" />
               </div>
-              <button className="mt-4 bg-white text-pink-400 font-bold py-2 px-4 rounded-lg">
-                Gráfico
-              </button>
+            ) : (
+              <div className="h-80 bg-gray-200 rounded-lg flex items-center justify-center">
+                {/* Imagen por defecto */}
+              </div>
+            )}
+          </div>
+
+          <div className="flex flex-col w-1/2 p-2 text-center" style={{ backgroundColor: '#EA95C4', color: 'white', fontFamily: 'DM Sans, sans-serif', width: '400px', height: '400px' }}>
+            <div className="text-lg font-semibold">Dashboard</div>
+            <div className="text-4xl mt-2"> Ki-67 Positivos {result?.positivos} </div>
+            <div className="text-4xl mt-2"> Ki-67 Negativos {result?.negativos} </div>
+            <div className="text-4xl mt-2">
+              Células positivas {Math.round((result?.positivos / (result?.positivos + result?.negativos)) * 100)}%
             </div>
           </div>
+        </div>
 
-          <div className="flex justify-center space-x-4 mt-4">
-          <input type="file" onChange={uploadFile}/>
-          <label className="cursor-pointer flex items-center">
-              <input
-                id="file-input"
-                type="file"
-                accept="image/*"
-                className="hidden"
-                onChange={handleImageUpload}
-              />
-            </label>
-            <button
-              className="flex items-center"
+        {/* Botones de subir y eliminar imagen */}
+        <div className="flex justify-start space-x-16 mt-4" style={{ marginLeft: "-50%", marginTop: "120px" }}> {/* Ajusta aquí los márgenes */}
+          <div className="flex flex-col items-center">
+            <img
+              src="/imagenes/Subir Imagen .png"
+              alt="Subir Imagen"
+              className="w-25 h-25 cursor-pointer" // Cambiar a w-32 y h-32 para un tamaño consistente
+              onClick={() => document.getElementById('file-input')?.click()}
+            />
+            <input
+              id="file-input"
+              type="file"
+              className="hidden"
+              onChange={uploadFile}
+            />
+          </div>
+
+          <div className="flex flex-col items-center">
+            <img
+              src="/imagenes/Eliminar Img.png"
+              alt="Eliminar Imagen"
+              className="w-25 h-25 cursor-pointer" // Cambiar a w-32 y h-32 para un tamaño consistente
               onClick={handleImageRemove}
-            >
-              <img
-                src="/imagenes/Eliminar Img.png" // Ruta de la imagen para eliminar
-                alt="Eliminar Imagen"
-                className="w-12 h-12 mr-2"
-              />
-            </button>
+            />
           </div>
         </div>
 
-        <div className="mt-4 text-center">
-          <div className="text-6xl font-bold text-pink-400">IACM</div>
-          <div className="text-xl">Plataforma Web</div>
-        </div>
       </main>
     </div>
   );
