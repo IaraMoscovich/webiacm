c5e53b53d683644fa12ef82672d638b35b36cae0
IaraMoscovich <110687618+IaraMoscovich@users.noreply.github.com>
2024-11-25 18:34:52 -0300

    cambie la conex a render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,9 +37,9 @@ const DashboardPage: React.FC = () => {
 
     const formData = new FormData();
     formData.append("file", file);
-
+//http://localhost:8000/upload-image/
     try {
-      const response = await fetch("http://localhost:8000/upload-image/", {
+      const response = await fetch("https://fastapi-example-endl.onrender.com/upload-image/", {
         method: "POST",
         body: formData,
       });
